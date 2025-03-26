33419130184bff871df10a561219a93f431503ea
qamosu <bahorin.ivan@gmail.com>
2025-03-26 07:08:27 +0100

    Initial Commit

---
+++ ClipboardHistory/ClipboardHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardHistoryApp.swift
+//  ClipboardHistory
+//
+//  Created by Ivan Bahorin on 26/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipboardHistoryTests/ClipboardHistoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardHistoryTests.swift
+//  ClipboardHistoryTests
+//
+//  Created by Ivan Bahorin on 26/03/2025.
+//
+
+import Testing
+@testable import ClipboardHistory
+
+struct ClipboardHistoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
